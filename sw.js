be74645845f01ebf0813ffe9734a79d9ea993ec3
AlekDrezce <alekdz74@gmail.com>
2026-09-24 23:08:33 +0000

    mass update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Guarda la app para que abra rápido y sin conexión; siempre intenta traer la versión más nueva primero.
-const CACHE = 'valknut-v1';
-const SHELL = ['./', './index.html', './config.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
+const CACHE = 'valknut-v2';
+const SHELL = ['./', './index.html', './config.js', './manifest.webmanifest', './icons/favicon.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
